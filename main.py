7a856fbb9e747e59fde6f04ec31135fd79c3a2fc
ruhaan08 <101627082+ruhaan08@users.noreply.github.com>
2022-03-15 13:57:21 +0530

    Add files via upload
    celcius to farenheight conversion.

---
+++ main.py
@@ -0,0 +1,22 @@
+tempreture = float(input('What is the tempreture in celcius? '))
+farenheight = (tempreture * 1.8) + 32
+if (farenheight > 100):
+    print('It is a hot day')
+elif (farenheight < 50):
+    print(' It is a cold day')
+elif (farenheight >= 50 and farenheight <= 100):
+    print('It is a nice day')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
